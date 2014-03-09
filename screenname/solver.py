4fc085edb2d632b295c2b28fffde25569081d860
Alex Whitney <aw1209@ic.ac.uk>
2014-03-08 20:46:04 -0500

    scrnsolver

---
+++ screenname/solver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def solve_it():
+    # return a string that is the screen name you would like
+    return 'adwhit'
+
+if __name__ == '__main__':
+    print solve_it()
+
