539df9c2c1278ed68ac27a1b74685c7655692e89
Alex Whitney <aw1209@ic.ac.uk>
2014-03-12 20:59:51 -0400

    graph ground work

--- coloring/graph.c
+++ coloring/graph.c
@@ -1,3 +1,11 @@
+//strategy: the colours constraint is returning the list in
+//the wrong order - fix
+//Also there is no copying etc going on yet, fix also
+//No ffi, that can be last?
+//Debug/verbose mode
+//Come up with some decent backtracking rules
+//Code is reasonably modular and logical but could be improved
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,10 +28,23 @@ typedef struct {
     Node *nodes;
 } Graph;
 
+typedef struct {
+    int itercount;
+} SolveState;
+
+void print_colours(Graph *g);
+
+void print_arr(int *arr, int len) {
+    for (int i=0;i<len;i++) {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
 int cmp_node_nlinks(const void *v1, const void *v2) {
     const Node *i1 = *(const Node **)v1;
     const Node *i2 = *(const Node **)v2;
-    return i1->nlinks < i2->nlinks ? -1 : (i1->nlinks > i2->nlinks);
+    return i1->nlinks > i2->nlinks ? -1 : (i1->nlinks < i2->nlinks);
 }
 
 int *node_argsort_nlinks(Node *array) {
@@ -32,12 +53,15 @@ int *node_argsort_nlinks(Node *array) {
     for(int i = 0; i < NNODES; i++) parray[i] = &array[i];
     qsort(parray, NNODES, sizeof *parray, cmp_node_nlinks);
     for(int i = 0;i < NNODES;i++) position[i] = (parray[i] - array);
-    free(parray);
     return position;
 }
 
 int *find_most_constrained(Graph *g) {
     int *position = node_argsort_nlinks(g->nodes);
+    puts("Node constraint order");
+    for (int i=0;i < NNODES; i++) {
+        printf("index: %d  Rank %d\n", i, position[i]);
+    }
     return position;
 }
 
@@ -47,35 +71,39 @@ int cmp_colour_freq(const void *v1, const void *v2) {
     return i1 < i2 ? -1 : (i1 > i2);
 }
 
-int *colour_freq_sort(int *array) {
-    int **parray = malloc(NNODES * sizeof(int));
-    int *position = malloc(NNODES * sizeof(int));
-    for(int i = 0; i < NNODES; i++) parray[i] = &array[i];
-    qsort(parray, NNODES, sizeof *parray, cmp_colour_freq);
-    for(int i = 0;i < NNODES;i++) position[i] = (parray[i] - array);
-    free(parray);
+int *colour_freq_sort(int *array, int ncolours) {
+    int **parray = malloc(ncolours * sizeof(int));
+    int *position = malloc(ncolours * sizeof(int));
+    for(int i = 0; i < ncolours; i++) parray[i] = &array[i];
+    qsort(parray, ncolours, sizeof *parray, cmp_colour_freq);
+    for(int i = 0;i < ncolours;i++) position[i] = (parray[i] - array);
     return position;
 }
 
-int *find_rarest_colour(Graph *g) {
-    //implicitly takes "first" rarest colour
+int *find_rarest_colours(Graph *g) {
     int *counts = calloc(g->ncolours, sizeof(int));
-    for (int i=0;i<NNODES;i++) {
+    for (int i=0;i<g->ncolours;i++) {
         for (int j=0;j<g->ncolours;j++) {
             if (g->nodes[i].posscolours[j]) counts[j]++;
         }
     }
-    int *positions = colour_freq_sort(counts);
+    int *positions = colour_freq_sort(counts, g->ncolours);
+    puts("color count ranking");
+    for (int i=0;i < g->ncolours; i++) {
+        printf("Index: %d Count: %d  Rank %d\n",i, counts[i], positions[i]);
+    }
     return positions;
 }
 
 Graph construct_graph(int nitems, int *n1inds,int *n2inds) {
-    Graph g = {0, calloc(NNODES, sizeof(int)), calloc(NNODES,sizeof(Node))};
+    Graph g = {4, calloc(NNODES, sizeof(int)), calloc(NNODES,sizeof(Node))};
     for (int i=0;i<NNODES;i++) {
+        g.node_colours[i] = -1;
         Node n = { i, 0,
             calloc(NNODES,sizeof(bool)),
             calloc(NNODES,sizeof(Node *))
         };
+        for (int j=0;j<NNODES;j++) n.posscolours[j] = true;
         g.nodes[i] = n;
     };
     for (int i=0;i<nitems;i++) {
@@ -104,23 +132,63 @@ bool set_and_propagate(int next_node_ix, int next_node_colour, Graph *g) {
         Node *linkednode = node.links[j];
         linkednode->posscolours[next_node_colour] = false;
     }
+    print_colours(g);
     return true;
 }
 
+void choose_next(int *next_node, int *next_colour, Graph *g) {
+    int *constrained_order = find_most_constrained(g);
+    int *rarest_colours = find_rarest_colours(g);
+    int nodeind = 0;
+    int colourind = 0;
+    // make sure node hasn't already been set
+    while (g->node_colours[constrained_order[nodeind]] != -1){ nodeind ++; }
+    // make sure node colour is possible
+    while (!g->nodes[nodeind].posscolours[rarest_colours[colourind]]) {
+        colourind++;
+        printf("colorind %d\n", colourind);
+    }
+    *next_node = constrained_order[nodeind];
+    *next_colour = rarest_colours[colourind];
+}
 
-
-bool solver(int next_node_ix, int next_node_colour, Graph g, int ncoloured) {
-    bool success = set_and_propagate(next_node_ix, next_node_colour, &g);
+bool solver(int next_node_ix, int next_node_colour, Graph *g, int ncoloured) {
+    bool success = set_and_propagate(next_node_ix, next_node_colour, g);
     if (!success) return false;
     if (ncoloured == NNODES) return true;
-    return true;
-        
-
+    int next_node;
+    int next_colour;
+    choose_next(&next_node, &next_colour, g);
+    solver(next_node, next_colour, g, ncoloured +1);
+    return false;
 }
 
-void solve(Graph g) {
-    int *constrained_order = find_most_constrained(&g);
+void solve(Graph *g) {
+    int *constrained_order = find_most_constrained(g);
+    printf("most constrained: %d \n", constrained_order[0]);
     solver(constrained_order[0], 0, g, 1);
+}
+
+void print_colours(Graph *g) {
+    printf("Colours: ");
+    for (int i=0;i<NNODES;i++) {
+        if (g->node_colours[i]==-1) {
+            printf(". ");
+        } else {
+            printf("%d ", g->node_colours[i]);
+        }
+    }
+    puts("");
+}
+
+void print_links(Graph *g) {
+    for (int i=0;i<NNODES;i++) {
+        printf("Node %d: ", i);
+        Node n = g->nodes[i];
+        for (int j=0;j<n.nlinks;j++)
+            printf("%d ", n.links[j]->index);
+        puts("");
+    }
 }
 
 int main() {
@@ -128,12 +196,9 @@ int main() {
     int n1ind[3] = {0,1,1};
     int n2ind[3] = {1,2,3};
     Graph graph = construct_graph(3, n1ind, n2ind);
-    for (int i=0;i<NNODES;i++) {
-        printf("Node %d: ", i);
-        Node n = graph.nodes[i];
-        for (int j=0;j<n.nlinks;j++)
-            printf("%d ", n.links[j]->index);
-        puts("");
-    }
+    print_links(&graph);
+    print_colours(&graph);
+    solve(&graph);
+    print_colours(&graph);
 }
 
