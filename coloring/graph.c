2baecc43cfabb90d13c29526029e4fbc1d662394
Alex Whitney <aw1209@ic.ac.uk>
2014-03-13 22:33:41 -0400

    Bug free, but serious optimisation required

--- coloring/graph.c
+++ coloring/graph.c
@@ -5,12 +5,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int NNODES = 0;
 
 typedef enum {false, true} bool;
-
 typedef struct Node Node;
+
+struct Tuple {
+    int *arr1;
+    int *arr2;
+};
 
 struct Node {
     int index;
@@ -30,8 +34,16 @@ typedef struct {
     int itercount;
 } SolveState;
 
+int NNODES = 0;
+int NEDGES = 0;
+
+bool VERBOSE = false;
+bool DEBUG = false;
+bool PRINTOUT = false;
+
 void print_colours(Graph *g);
 void print_colour_state(Graph *g);
+void print_output(Graph *g);
 
 int cmp_node_nlinks(const void *v1, const void *v2) {
     const Node *i1 = *(const Node **)v1;
@@ -40,11 +52,12 @@ int cmp_node_nlinks(const void *v1, const void *v2) {
 }
 
 int *node_argsort_nlinks(Node *array) {
-    Node **parray = malloc(NNODES * sizeof(Node));
+    Node **parray = malloc(NNODES * sizeof(Node *));
     int *position = malloc(NNODES * sizeof(int));
     for(int i = 0; i < NNODES; i++) parray[i] = &array[i];
-    qsort(parray, NNODES, sizeof *parray, cmp_node_nlinks);
+    qsort(parray, NNODES, sizeof(Node *), cmp_node_nlinks);
     for(int i = 0;i < NNODES;i++) position[i] = (parray[i] - array);
+    free(parray);
     return position;
 }
 
@@ -58,36 +71,45 @@ int *find_most_constrained(Graph *g) {
 }
 
 int cmp_colour_freq(const void *v1, const void *v2) {
-    const int *i1 = *(const int **)v1;
-    const int *i2 = *(const int **)v2;
-    return i1 > i2 ? -1 : (i1 < i2);
+    const int i1 = **(const int **)v1;
+    const int i2 = **(const int **)v2;
+    if (i1 < i2) return -1;
+    if (i1 > i2) return 1;
+    return 0;
 }
 
 int *colour_freq_sort(int *array, int ncolours) {
-    int **parray = malloc(ncolours * sizeof(int));
+    int **parray = malloc(ncolours * sizeof(int *));
     int *position = malloc(ncolours * sizeof(int));
     for(int i = 0; i < ncolours; i++) parray[i] = &array[i];
-    qsort(parray, ncolours, sizeof *parray, cmp_colour_freq);
+    qsort(parray, ncolours, sizeof(int *), cmp_colour_freq);
+    if (DEBUG) for (int i=0;i<ncolours;i++) {
+        printf("after parray val %p, array val %p position %li\n", parray[i], array, parray[i] - array);
+    }
     for(int i = 0;i < ncolours;i++) position[i] = (parray[i] - array);
+    free(parray);
     return position;
 }
 
 int *find_rarest_colours(Graph *g) {
     int *counts = calloc(g->ncolours, sizeof(int));
-    for (int i=0;i<g->ncolours;i++) {
+    for (int i=0;i<NNODES;i++) {
         for (int j=0;j<g->ncolours;j++) {
             if (g->nodes[i].posscolours[j]) counts[j]++;
         }
     }
     int *positions = colour_freq_sort(counts, g->ncolours);
-    /*puts("color count ranking");*/
-    /*for (int i=0;i < g->ncolours; i++) {*/
-        /*printf("Index: %d Count: %d  Rank %d\n",i, counts[i], positions[i]);*/
-    /*}*/
+    if (DEBUG) {
+        puts("color count ranking");
+        for (int i=0;i < g->ncolours; i++) {
+            printf("Rank: %d Index: %d  Counts %d\n",i, positions[i], counts[positions[i]]);
+        }
+    }
+    free(counts);
     return positions;
 }
 
-Graph construct_graph(int ncolours, int nitems, int *n1inds,int *n2inds) {
+Graph construct_graph(int ncolours, int *n1inds,int *n2inds) {
     //initialise graph and nodes. Possible cffi entry point
     Graph g = {ncolours, calloc(NNODES, sizeof(int)), calloc(NNODES,sizeof(Node))};
     for (int i=0;i<NNODES;i++) {
@@ -99,7 +121,7 @@ Graph construct_graph(int ncolours, int nitems, int *n1inds,int *n2inds) {
         for (int j=0;j<NNODES;j++) n.posscolours[j] = true;
         g.nodes[i] = n;
     };
-    for (int i=0;i<nitems;i++) {
+    for (int i=0;i<NEDGES;i++) {
         int n1ind = n1inds[i];
         int n2ind = n2inds[i];
         Node *n1 = &g.nodes[n1ind];
@@ -112,12 +134,22 @@ Graph construct_graph(int ncolours, int nitems, int *n1inds,int *n2inds) {
     return g;
 }
 
+void free_graph(Graph *g) {
+    free(g->node_colours);
+    for (int i=0;i<NNODES;i++) {
+        free(g->nodes[i].posscolours);
+        free(g->nodes[i].links);
+    };
+    free(g->nodes);
+};
+
+
 bool set_and_propagate(int next_node_ix, int next_node_colour, Graph *g) {
-    printf("Setting node %d to colour %d\n", next_node_ix, next_node_colour);
+    if (VERBOSE) printf("Setting node %d to colour %d\n", next_node_ix, next_node_colour);
     //set
-    Node node = g->nodes[next_node_ix];
     g->node_colours[next_node_ix] = next_node_colour;
     //propagate - remove colour from nearby nodes
+    Node node = g->nodes[next_node_ix];
     for (int j=0;j<node.nlinks;j++) {
         Node *linkednode = node.links[j];
         if (linkednode->posscolours[next_node_colour]) {
@@ -126,7 +158,6 @@ bool set_and_propagate(int next_node_ix, int next_node_colour, Graph *g) {
             if (linkednode->nposscolours <= 0) return false;
         }
     }
-    print_colour_state(g);
     return true;
 }
 
@@ -141,10 +172,13 @@ void choose_next(int *next_node, int *next_colour, Graph *g) {
     while (!g->nodes[constrained_order[nodeind]].posscolours[rarest_colours[colourind]]) colourind++;
     *next_node = constrained_order[nodeind];
     *next_colour = rarest_colours[colourind];
+    free(constrained_order);
+    free(rarest_colours);
 }
 
 bool solver(int next_node_ix, int next_node_colour, Graph *g, int ncoloured) {
     bool success = set_and_propagate(next_node_ix, next_node_colour, g);
+    if (VERBOSE) print_colour_state(g);
     if (!success) return false;
     else if (ncoloured == NNODES) return true;
     else {
@@ -156,25 +190,31 @@ bool solver(int next_node_ix, int next_node_colour, Graph *g, int ncoloured) {
 }
 
 bool start_solver(Graph *g) {
-    printf("Attempting to solve with %d colours\n", g->ncolours);
+    if (VERBOSE) printf("Attempting to solve with %d colours\n", g->ncolours);
     int *constrained_order = find_most_constrained(g);
     bool success = solver(constrained_order[0], 0, g, 1);
-    if (success) puts("Success!");
-    else puts("Fail");
+    if (VERBOSE) {
+        if (success) puts("Success!");
+        else puts("Fail");
+    }
     return success;
 }
 
-void solve(int nitems, int *n1ind, int *n2ind) {
+void solve(int *n1ind, int *n2ind) {
     // TODO: free graph on failure
     int ncolours = 2;
     bool success = false;
+    Graph graph;
     while (!success) {
-        Graph graph = construct_graph(ncolours, nitems, n1ind, n2ind);
+        graph = construct_graph(ncolours, n1ind, n2ind);
         success = start_solver(&graph);
+        if (success) break;
+        free_graph(&graph);
         ncolours++;
     }
+    if (PRINTOUT) print_colour_state(&graph);
+    print_output(&graph);
 }
-
 
 void print_colours(Graph *g) {
     printf("Colours: ");
@@ -188,7 +228,7 @@ void print_colours(Graph *g) {
 void print_colour_state(Graph *g) {
     printf("Colour state:\n");
     for (int i=0;i<NNODES;i++) {
-        printf("Node %d: | ", i);
+        printf("Node %02d: | ", i);
         for (int j=0;j<g->ncolours;j++) {
             if (g->nodes[i].posscolours[j]) printf("0 ");
             else printf(". ");
@@ -208,10 +248,39 @@ void print_links(Graph *g) {
     }
 }
 
-int main() {
-    NNODES = 10;
-    int n1ind[13] = {0,1,1,1,3,5,6,6,5,9,9,9,9};
-    int n2ind[13] = {1,2,3,4,4,6,7,8,4,1,2,3,4};
-    solve(13, n1ind, n2ind);
+void print_output(Graph *g) {
+    printf("%d 0\n", g->ncolours);
+    for (int i=0;i<NNODES;i++) printf("%d ",g->node_colours[i]);
+    puts("");
 }
 
+struct Tuple parse_file(char *fpath) {
+    FILE *myfile = fopen (fpath,"r");
+    if (myfile == NULL) {
+        perror ("Error opening file"); //or return 1;
+        exit(1);
+    }
+    fscanf(myfile, "%d %d", &NNODES, &NEDGES);
+    int *n1ind = malloc(NEDGES * sizeof(int));
+    int *n2ind = malloc(NEDGES * sizeof(int));
+    for (int i=0; i<(NEDGES); i++) {
+        fscanf(myfile, "%d %d", &n1ind[i], &n2ind[i]);
+    }
+    fclose(myfile);
+    struct Tuple tup = {n1ind, n2ind};
+    return tup;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("No file name specified\n");
+        exit(1);
+    }
+    for (int i=1;i<argc;i++) {
+        if (strcmp(argv[i],"-v") == 0) VERBOSE = true;
+        if (strcmp(argv[i],"-d") == 0) DEBUG = true;
+        if (strcmp(argv[i],"-p") == 0) PRINTOUT = true;
+    }
+    struct Tuple tup = parse_file(argv[1]);
+    solve(tup.arr1, tup.arr2);
+}
---
+++ coloring/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./graph data/gc_50_3
+./graph data/gc_70_7
+./graph data/gc_100_5
+./graph data/gc_250_9
+./graph data/gc_500_1
+./graph data/gc_1000_5
